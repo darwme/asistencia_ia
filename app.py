bc97afc714de60e9127b27461adf2d18ebb4b271
MichaelChirinos <166664629+MichaelChirinos@users.noreply.github.com>
2025-02-27 14:10:32 -0500

    Add CORS configuration to app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
 db = SQLAlchemy(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
